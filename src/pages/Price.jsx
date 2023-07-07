a429ab048d3f175660eb643d1cf7a74d434e87a4
Ethmane Didi <ousman.didibela@gmail.com>
2023-07-07 13:33:08 +0200

    Update Price component with responsive and centered content

--- src/pages/Price.jsx
+++ src/pages/Price.jsx
@@ -4,15 +4,19 @@ import { Link } from 'react-router-dom';
 const Price = () => {
   return (
     <div className='flex flex-col items-center justify-center h-screen'>
-      <h1 className='text-4xl mb-8'>Kontakt oss for prisinformasjon</h1>
-      <p className='text-xl mb-8'>
+      <h1 className='text-4xl mb-8 text-center'>
+        Kontakt oss for prisinformasjon
+      </h1>
+      <p className='text-xl mb-8 text-center'>
         Ønsker du å få informasjon om priser? Ta kontakt med oss!
       </p>
-      <Link
-        to='/contact'
-        className='px-6 py-3 bg-red-500 text-white rounded hover:bg-red-600 transition duration-300'>
-        Kontakt oss
-      </Link>
+      <div className='flex justify-center'>
+        <Link
+          to='/contact'
+          className='px-6 py-3 bg-red-500 text-white rounded hover:bg-red-600 transition duration-300'>
+          Kontakt oss
+        </Link>
+      </div>
     </div>
   );
 };
